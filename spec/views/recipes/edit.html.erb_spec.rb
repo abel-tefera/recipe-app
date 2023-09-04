8d7c9ff8e1fbf469c2751e83c916b0948a0c03c0
abel-tefera <abeltefera16@gmail.com>
2023-09-04 14:23:55 +0300

    Recipes pages setup

--- spec/views/recipes/edit.html.erb_spec.rb
+++ spec/views/recipes/edit.html.erb_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "recipes/edit", type: :view do
-  let(:recipe) {
-    Recipe.create!()
-  }
+RSpec.describe 'recipes/edit', type: :view do
+  let(:recipe) do
+    Recipe.create!
+  end
 
   before(:each) do
     assign(:recipe, recipe)
   end
 
-  it "renders the edit recipe form" do
+  it 'renders the edit recipe form' do
     render
 
-    assert_select "form[action=?][method=?]", recipe_path(recipe), "post" do
+    assert_select 'form[action=?][method=?]', recipe_path(recipe), 'post' do
     end
   end
 end
