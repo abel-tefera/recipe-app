a00f9210ee4463f306eb7b5faf419b2fd8d84536
abel-tefera <abeltefera16@gmail.com>
2023-09-06 08:55:20 +0300

    Updates based on feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :recipes
   devise_for :users
   
   resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :new, :create, :destroy, :show]
 
   root to: "home#index" 
 end
