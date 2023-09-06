a29595c2cb8d64f086186a948bc669bb28d498f6
abel-tefera <abeltefera16@gmail.com>
2023-09-06 07:56:17 +0300

    Generate recipe_foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/destroy'
   resources :recipes
   devise_for :users
   
