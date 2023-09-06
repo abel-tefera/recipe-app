4dca590f90fbde15e021d8a1d4708d91262bc869
abel-tefera <abeltefera16@gmail.com>
2023-09-06 13:25:15 +0300

    Linters fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :rememberable, :validatable
+         :rememberable, :validatable
 
   has_many :foods
   has_many :recipes
