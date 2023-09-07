22b1f7a59f32d33fb8eb9034a76dcd93f3c4340b
abel-tefera <abeltefera16@gmail.com>
2023-09-07 11:23:38 +0300

    Shopping list done

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -7,7 +7,18 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = RecipeFood.new(recipe_food_parms)
     @recipe_food.recipe_id = params[:recipe_id]
 
-    if @recipe_food.save
+    @existing_recipe_food = RecipeFood.where(recipe_id: params[:recipe_id], food_id: @recipe_food.food_id)
+
+    if @existing_recipe_food.exists?
+      @existing_recipe_food.first.quantity = @recipe_food.quantity
+      if @existing_recipe_food.first.save
+        flash[:notice] = 'Ingredient has been added'
+        redirect_to "/recipes/#{params[:recipe_id]}"
+      else
+        flash[:error] = 'An error has occured'
+        render :new, status: 422
+      end
+    elsif @recipe_food.save
       flash[:notice] = 'Ingredient has been added'
       redirect_to "/recipes/#{params[:recipe_id]}"
     else
--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,4 +1,9 @@
 class ShoppingListController < ApplicationController
   def index
+    all_public_recipe_foods = Recipe.where(public: true).includes(recipe_foods: [:food]).map(&:recipe_foods)
+      .flatten.map(&:food)
+    all_user_foods = Food.where(user: current_user)
+
+    @missing_foods = all_public_recipe_foods.reject { |food| all_user_foods.include?(food) }
   end
 end
