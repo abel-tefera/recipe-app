88b3e56c7c3c89dd82cf7f703c51a518fb65217a
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-09-07 12:16:15 +0500

    Add Public Recipes Page In Recipe App

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipe_all = Recipe.where(public: true).includes(:user)
+    @recipe_all = Recipe.where(public: true).includes(:user).order(created_at: :desc)
   end
 end
