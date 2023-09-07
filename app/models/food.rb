3ba91a52296c39888a0fa47b3f2550a4095b0d03
abel-tefera <abeltefera16@gmail.com>
2023-09-07 13:30:39 +0300

    Model tests for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,4 +5,5 @@ class Food < ApplicationRecord
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
