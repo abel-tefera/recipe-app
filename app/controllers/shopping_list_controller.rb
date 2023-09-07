4314cc3fc704a54d0884e76de1cf8d61d16b062a
abel-tefera <abeltefera16@gmail.com>
2023-09-07 11:39:08 +0300

    Amount & total for shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -5,5 +5,11 @@ class ShoppingListController < ApplicationController
     all_user_foods = Food.where(user: current_user)
 
     @missing_foods = all_public_recipe_foods.reject { |food| all_user_foods.include?(food) }
+    @amt = @missing_foods.length
+    @total = @missing_foods.map do |food|
+      food.price * food.recipe_foods.select do |recipe|
+                     recipe.food_id == food.id
+                   end.first.quantity
+    end.sum
   end
 end
